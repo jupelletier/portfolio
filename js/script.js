90faf843002fe01a061e178067ebb3aaf9779697
Julien Pelletier <julien.pelletier88@gmail.com>
2014-05-16 16:16:27 +0200

    Setting up

---
+++ js/script.js
@@ -0,0 +1,21 @@
+jQuery(document).ready(function($){
+  
+	var container = document.querySelector('#lec-misc-list');
+	var msnry = new Masonry( container, {
+	// options
+
+	itemSelector: '.item',
+	gutter: 50
+	});
+})
+
+jQuery(document).ready(function($){
+  
+	var container = document.querySelector('#cookening-misc-list');
+	var msnry = new Masonry( container, {
+	// options
+
+	itemSelector: '.item',
+	gutter: 50
+	});
+})
